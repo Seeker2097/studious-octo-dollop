a14ce2ae11cd3a89925fa19866a54c98f077c934
Seeker2097 <99454568+Seeker2097@users.noreply.github.com>
2022-02-13 23:01:42 -0500

    Add files via upload

---
+++ Pong.py
@@ -0,0 +1,125 @@
+import turtle
+#Screen settings
+sc = turtle.Screen()
+sc.title("Pong")
+sc.bgcolor("black")
+sc.setup(width=1000, height=600)
+#left paddle settings
+
+left_paddle = turtle.Turtle()
+left_paddle.speed(0)
+left_paddle.shape("square")
+left_paddle.color("white")
+left_paddle.shapesize(stretch_wid=6, stretch_len=2)
+left_paddle.penup()
+left_paddle.goto(-400, 0)
+
+#right paddle settings
+right_paddle = turtle.Turtle()
+right_paddle.speed(0)
+right_paddle.shape("square")
+right_paddle.color("white")
+right_paddle.shapesize(stretch_wid=6, stretch_len=2)
+right_paddle.penup()
+right_paddle.goto(400, 0)
+
+#ball settings
+hit_ball = turtle.Turtle()
+hit_ball.speed(40)
+hit_ball.shape('circle')
+hit_ball.color('white')
+hit_ball.penup()
+hit_ball.goto(0, 0)
+hit_ball.dx = 5
+hit_ball.dy = -5
+
+#scoreboard
+left_player = 0
+right_player = 0
+#displays the scoreboard
+sketch = turtle.Turtle()
+sketch.speed(0)
+sketch.color("white")
+sketch.penup()
+sketch.hideturtle()
+sketch.goto(0, 260)
+sketch.write("Left_Player : 0     Right_Player: 0",
+             align="center", font=("Courier", 24, "normal"))
+
+# Function to move paddle verticlly
+def paddleaup():
+    y = left_paddle.ycor()
+    y += 20
+    left_paddle.sety(y)
+
+def paddleadown():
+    y = left_paddle.ycor()
+    y -= 20
+    left_paddle.sety(y)
+
+def paddlebup():
+    y = right_paddle.ycor()
+    y += 20
+    right_paddle.sety(y)
+
+def paddlebdown():
+    y = right_paddle.ycor()
+    y -= 20
+    right_paddle.sety(y)
+
+# keyboard bindings
+sc.listen()
+sc.onkeypress(paddleaup, "e")
+sc.onkeypress(paddleadown, "x")
+sc.onkeypress(paddlebup, "Up")
+sc.onkeypress(paddlebdown, "Down")
+
+while True:
+    sc.update()
+
+    hit_ball.setx(hit_ball.xcor()+hit_ball.dx)
+    hit_ball.sety(hit_ball.ycor()+hit_ball.dy)
+
+# Checking borders
+    if hit_ball.ycor() > 280:
+        hit_ball.sety(280)
+        hit_ball.dy *= -1
+    if hit_ball.ycor() < -280:
+        hit_ball.sety(-280)
+        hit_ball.dy *= -1
+    if hit_ball.xcor() > 500:
+        hit_ball.goto(0, 0)
+        hit_ball.dy *= -1
+        left_player += 1
+        sketch.clear()
+        sketch.write("Left_Player : {}     Right_Player : {}".format(
+            left_player, right_player), align="center",
+            font=("Courier", 24, "normal"))
+
+    if hit_ball.xcor() < -500:
+        hit_ball.goto(0, 0)
+        hit_ball.dy *= -1
+        right_player += 1
+        sketch.clear()
+        sketch.write("Left_Player : {}     Right_Player: {}".format(
+        left_player, right_player), align="center",
+        font=("Courier", 24, "normal"))
+
+    # paddle ball collision
+    if (hit_ball.xcor() > 360 and hit_ball.xcor() < 370) and (hit_ball.ycor() < right_paddle.ycor() + 40 and hit_ball.ycor() > right_paddle.ycor() - 40):
+        hit_ball.setx(360)
+        hit_ball.dx *= -1
+
+    if (hit_ball.xcor() < -360 and hit_ball.xcor() > -370) and (hit_ball.ycor() < left_paddle.ycor() + 40 and hit_ball.ycor() > left_paddle.ycor() - 40):
+        hit_ball.setx(-360)
+        hit_ball.dx *= -1
+
+
+
+# keyboard bindings
+
+
+
+
+#turtle.done() is used to keep the window from closing.
+turtle.done()
